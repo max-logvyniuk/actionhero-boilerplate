531f2f3999d41b2ef5500bcd8f6ebe15c082e662
Max Logvynyuk <maxlogvynyuk@gmail.com>
2021-04-14 19:39:07 +0300

    fixed for example

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,15 +3,17 @@ import { join } from 'path';
 
 export const DEFAULT = {
   sequelize: (config) => {
-    let dialect = 'mariadb';
+    let dialect = 'mysql';
     let host = process.env.DB_HOST || '127.0.0.1';
     let port = process.env.DB_PORT || '3306';
-    let database = process.env.DB_NAME || 'mariadb_actionhero';
+    let database = process.env.DB_NAME || 'ubki';
     let username = process.env.DB_USER || process.env.CI ? process.env.DB_USER : 'root';
-    let password = process.env.DB_PASS || 'root';
+    let password = process.env.DB_PASS;
 
     // if your environment provides database information via a single JDBC-style URL like mysql://username:password@hostname:port/default_schema
-    const connectionURL = process.env.DATABASE_URL || process.env.MYSQL_URL || process.env.PG_URL;
+    const connectionURL = process.env.DATABASE_URL ||
+      process.env.MYSQL_URL ||
+      process.env.PG_URL;
 
     if (connectionURL) {
       const parsed = new URL(connectionURL);
@@ -35,24 +37,48 @@ export const DEFAULT = {
       username: username,
       password: password || 'root',
       models: [join(__dirname, '..', 'models')],
-      migrations: [join(__dirname, '..', 'migrations')]
+      migrations: [join(__dirname, '..', 'migrations')],
+      dialectOptions: {
+        // ssl: 'Amazon RDS'
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+      }
       // you can also pass "dialectOptions", for example if you need `{ssl: true}` for Postgres
     };
   }
 };
 
 // for the sequelize CLI tool
-module.exports.development = DEFAULT.sequelize({
-  env: 'development',
-  process: { env: 'development' }
-});
+// module.exports.development = DEFAULT.sequelize({
+//   env: 'development',
+//   process: { env: 'development' }
+// });
+//
+// module.exports.sandbox = DEFAULT.sequelize({
+//   env: 'sandbox',
+//   process: { env: 'sandbox' },
+//   database: '{DB_NAME}',
+//   host: '{DB_HOST}',
+//   port: '{DB_PORT}',
+//   username: '{DB_USERNAME}',
+//   password: '{DB_PASSWORD}',
+//   dialectOptions: {
+//     ssl: 'Amazon RDS'
+//   }
+// });
+//
+// module.exports.production = DEFAULT.sequelize({
+//   env: 'production',
+//   process: { env: 'production' },
+//   database: '{DB_NAME_PROD}',
+//   host: '{DB_HOST_PROD}',
+//   port: '{DB_PORT_PROD}',
+//   username: '{DB_USERNAME_PROD}',
+//   password: '{DB_PASSWORD_PROD}',
+//   dialectOptions: {
+//     ssl: 'Amazon RDS'
+//   }
+// });
 
-module.exports.staging = DEFAULT.sequelize({
-  env: 'staging',
-  process: { env: 'staging' }
-});
-
-module.exports.production = DEFAULT.sequelize({
-  env: 'production',
-  process: { env: 'production' }
-});
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,49 @@
 #!/usr/bin/env node
+const cluster = require('cluster');
+
 import { Process } from 'actionhero';
+import { cpus } from 'os';
+
+// const numberOfWorkers = cpus().length;
+const numberOfWorkers = process.env.NUMBER_OF_WORKERS || 2;
 
 // load any custom code, configure the env, as needed
 
 async function main() {
   // create a new actionhero process
-  const app = new Process();
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
 
-  // handle unix signals and uncaught exceptions & rejections
-  app.registerProcessSignals((exitCode) => {
-    process.exit(exitCode);
-  });
+    // Fork workers.
+    for (let i = 0; i < numberOfWorkers; i++) {
+      cluster.fork();
+    }
 
-  // start the app!
-  // you can pass custom configuration to the process as needed
-  await app.start();
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    const app = new Process();
+
+    console.info('sever app1 -----', app);
+
+    // handle unix signals and uncaught exceptions & rejections
+    app.registerProcessSignals((exitCode) => {
+      process.exit(exitCode);
+    });
+
+    console.log(`Worker ${process.pid} started`);
+
+    // start the app!
+    // you can pass custom configuration to the process as needed
+    try {
+      await app.start();
+    } catch (error) {
+      console.info('server app.start ----', error);
+    }
+
+    console.info('sever app2 -----', app);
+  }
 }
 
 main();
